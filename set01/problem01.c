013912511df45f7849b896e55ec8d68ed7a3257e
l0g3sh-s <logeshs.bsc23@rvu.edu.in>
2023-10-19 16:53:37 +0530

    first commit

--- set01/problem01.c
+++ set01/problem01.c
@@ -1,1 +1,10 @@
 // 1. Write a C program to print your name.
+#include<stdio.h>
+
+int main(){
+    char name[10];
+    printf("Enter your name\n");
+    scanf("%s",&name);
+    printf(name);
+    
+}
