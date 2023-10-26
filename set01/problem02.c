68dc06d48c56dd59685da400dd9300450f866ccc
l0g3sh-s <logeshs.bsc23@rvu.edu.in>
2023-10-26 15:46:05 +0530

    second commit

--- set01/problem02.c
+++ set01/problem02.c
@@ -1,2 +1,19 @@
 //2. Write a C program to add two numbers.
+#include <stdio.h>
+
+int main() {
+    double num1, num2, sum;
+
+    printf("Enter the first number: ");
+    scanf("%lf", &num1);
+
+    printf("Enter the second number: ");
+    scanf("%lf", &num2);
+
+    sum = num1 + num2;
+
+    printf("Sum: %lf\n", sum);
+
+    return 0;
+}
 
