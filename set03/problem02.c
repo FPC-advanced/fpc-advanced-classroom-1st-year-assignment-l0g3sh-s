dc3ccb0c88f05355257dc1650aeece1a2e4e9f27
l0g3sh-s <logeshs.bsc23@rvu.edu.in>
2023-11-06 10:28:56 +0530

    set3prb02

--- set03/problem02.c
+++ set03/problem02.c
@@ -1,48 +1,47 @@
 #include <stdio.h>
+#include <math.h>
 
-// Function to input the side lengths of the triangle
-int input_side() {
-    int side;
-    printf("Enter the length of a side: ");
-    scanf("%d", &side);
-    return side;
-}
-
-// Function to check if a triangle is scalene
-int check_scalene(int a, int b, int c) {
-    if (a != b && b != c && a != c) {
-        return 1; // It's a scalene triangle
-    } else {
-        return 0; // It's not a scalene triangle
-    }
-}
-
-// Function to display the side lengths and whether the triangle is scalene
-void output(int a, int b, int c, int isscalene) {
-    printf("Side 1: %d\n", a);
-    printf("Side 2: %d\n", b);
-    printf("Side 3: %d\n", c);
-
-    if (isscalene) {
-        printf("The triangle is scalene.\n");
-    } else {
-        printf("The triangle is not scalene.\n");
-    }
-}
+void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3);
+int is_triangle(float x1, float y1, float x2, float y2, float x3, float y3);
+void output(float x1, float y1, float x2, float y2, float x3, float y3, int result);
 
 int main() {
-    int side1, side2, side3;
-
-    // Input the side lengths
-    side1 = input_side();
-    side2 = input_side();
-    side3 = input_side();
-
-    // Check if it's a scalene triangle
-    int isscalene = check_scalene(side1, side2, side3);
-
-    // Display the result
-    output(side1, side2, side3, isscalene);
+    float x1, y1, x2, y2, x3, y3;
+    input_triangle(&x1, &y1, &x2, &y2, &x3, &y3);
+    int result = is_triangle(x1, y1, x2, y2, x3, y3);
+    output(x1, y1, x2, y2, x3, y3, result);
 
     return 0;
+}
+
+void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3) {
+    printf("Enter the coordinates of Point 1 (x1 y1): ");
+    scanf("%f %f", x1, y1);
+
+    printf("Enter the coordinates of Point 2 (x2 y2): ");
+    scanf("%f %f", x2, y2);
+
+    printf("Enter the coordinates of Point 3 (x3 y3): ");
+    scanf("%f %f", x3, y3);
+}
+
+int is_triangle(float x1, float y1, float x2, float y2, float x3, float y3) {
+    
+    float slope1 = (y2 - y1) / (x2 - x1);
+    float slope2 = (y3 - y2) / (x3 - x2);
+
+    if (slope1 != slope2) {
+        return 1;
+    }
+
+    
+    return 0;
+}
+
+void output(float x1, float y1, float x2, float y2, float x3, float y3, int result) {
+    if (result) {
+        printf("The points (%.2f, %.2f), (%.2f, %.2f), and (%.2f, %.2f) form a triangle.\n", x1, y1, x2, y2, x3, y3);
+    } else {
+        printf("The points (%.2f, %.2f), (%.2f, %.2f), and (%.2f, %.2f) do not form a triangle.\n", x1, y1, x2, y2, x3, y3);
+    }
 }
