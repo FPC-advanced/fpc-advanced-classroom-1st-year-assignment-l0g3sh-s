c6ed5c211c8e1e66065c1642d1d45d73ecf059ed
l0g3sh-s <logeshs.bsc23@rvu.edu.in>
2023-12-05 12:43:56 +0530

    prb01set5

---
+++ set05/problem01.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <math.h>
+
+struct _point {
+    float x;
+    float y;
+};
+
+typedef struct _point Point;
+
+Point input();
+void dist(Point a, Point b, float *res);
+void output(Point a, Point b, float res);
+
+int main() {
+    Point point1, point2;
+    float distance;
+
+    point1 = input();
+    point2 = input();
+
+    dist(point1, point2, &distance);
+    output(point1, point2, distance);
+
+    return 0;
+}
+
+Point input() {
+    Point p;
+    printf("Enter x coordinate: ");
+    scanf("%f", &p.x);
+    printf("Enter y coordinate: ");
+    scanf("%f", &p.y);
+    return p;
+}
+
+void dist(Point a, Point b, float *res) {
+    *res = sqrt(pow((b.x - a.x), 2) + pow((b.y - a.y), 2));
+}
+
+void output(Point a, Point b, float res) {
+    printf("Distance between (%.2f, %.2f) and (%.2f, %.2f) is %.2f\n",
+           a.x, a.y, b.x, b.y, res);
+}
